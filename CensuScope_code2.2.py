aeb5a653b91c625dedacf9dd16638a6c695b9ebc
James Ziegler <72983836+jamziegl@users.noreply.github.com>
2021-02-09 18:03:24 -0500

    Update CensuScope_code2.2.py
    Added documentation for "Troubleshooting" at the end of the script for common errors.
    Made some changes to the descriptions following variable assignments.

--- CensuScope_code2.2.py
+++ CensuScope_code2.2.py
@@ -7,9 +7,9 @@
 """
 
         Author(s); Lindsay Hopson and Charles Hadley King
-        Date: 02-05-2020
+        Date: 02-09-2021
         Title of program/source code: Unique Accession Organism Information Retrieval
-        Code version:2.0
+        Code version: 2.1
         Type: Python 3.8.0
 
 
@@ -18,7 +18,8 @@
         Date:<August 27, 2019>
         Code version:<1.0>
         Availibility:<https://github.com/GW-HIVE/microbiome>
-
+        
+        !!!IMORTANT: BEFORE RUNNING, COPY THIS SCRIPT FILE AND PASTE IT TO THE SAME FOLDER YOU SPECIFY ON LINE 39!!!
 
         Takes a list of accessions and returnes the Accession, Organsism name, and TaxID
 
@@ -26,16 +27,18 @@
         Biopython package (http://biopython.org/DIST/docs/tutorial/Tutorial.pdf). In order
         to utilize this package in Python, follow the intallation instructions provided in
         the README link: https://github.com/biopython/biopython/blob/master/README.rst
-
+         
 """
 ######################################################################################
 
 import time
 from Bio import Entrez
 
-Entrez.email = 'lhopson15@gwu.edu'#input your email
+Entrez.email = 'example@email.com'#input an email address
 
-in_file = '/Users/lindsayhopson/Documents/mouse_CensuScope_outputs/newAccList1.2.txt' #use your appropriate file path
+in_file = '/Users/username/directory/folder/filename.txt' #(Use your appropriate file path and
+                                                          #the name of the file that was 
+                                                          #generated after running CensuScope_code1.3)
 
 with open('orgNames_newAccList2.2.txt', 'w') as output_File:
 
@@ -57,5 +60,24 @@ with open('orgNames_newAccList2.2.txt', 'w') as output_File:
                         count = count + 1
                         print (i+'***'+a+'***'+ b+'***'+c)
                         time.sleep(1)
-
-
+######################################################################################
+"""
+Troubleshooting:
+-If given the error: "ModuleNotFoundError: No module named 'Bio'":
+          Be sure that Biopython is installed and up to date.        
+          Use <pip3 install biopython> in command prompt for best results.
+          See (https://biopython.org/wiki/Download) for more details. 
+          
+-If given the error: "urllib.error.URLError: <urlopen error 
+          [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: 
+          unable to get local issuer certificate (_ssl.c:1123)>" 
+          
+          And the output file is empty (0KB), 
+          then complete the following steps and then rerun code:
+          If you’re using macOS go to Macintosh HD > Applications > Python3.6 folder 
+          (or whatever version of python you’re using) 
+          > double click on “Install Certificates.command” file.
+          
+          See (https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org)
+          for more details.
+"""
